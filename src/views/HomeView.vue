83f311acba69358b5fe638d7e5a6cdaa02525257
woosoobin <166790070+woosoobin@users.noreply.github.com>
2024-05-08 17:51:41 +0900

    초록 버튼 생성, flex 주기

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,75 +1,147 @@
 <template>
-    <div> </div>
+
+   <div class="dropdown">
+    <div class="text-white text-center py-1" style="background-color: green; width: 50px;">학년 </div>
+  <button style="border-color: #bbbbc2;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    학년
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">1학년</a></li>
+    <li><a class="dropdown-item" href="#">2학년</a></li>
+    <li><a class="dropdown-item" href="#">3학년</a></li>
+  </ul>
+  </div>
+  <ul> </ul> 
+
+<div class="d-flex">
+<div class="dropdown">
+    
+  <button style="border-color: #bbbbc2;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    연도
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">2022</a></li>
+    <li><a class="dropdown-item" href="#">2023</a></li>
+    <li><a class="dropdown-item" href="#">2024</a></li>
+  </ul>
+  </div>
+  <div style="font-size: 25px; margin: 0 20x;">~</div>
+  <div class="dropdown">
+  <button style="border-color: #bbbbc2;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    연도
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">2022</a></li>
+    <li><a class="dropdown-item" href="#">2023</a></li>
+    <li><a class="dropdown-item" href="#">2024</a></li>
+  </ul>
+  </div>
+</div>
+<ul> </ul>
+
+
+    
     <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="all_month">
   <label class="form-check-label" for="inlineCheckbox1">전체</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month3">
   <label class="form-check-label" for="inlineCheckbox2">3월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month4">
   <label class="form-check-label" for="inlineCheckbox2">4월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month5">
   <label class="form-check-label" for="inlineCheckbox2">5월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month6">
   <label class="form-check-label" for="inlineCheckbox2">6월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month7">
   <label class="form-check-label" for="inlineCheckbox2">7월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month8">
   <label class="form-check-label" for="inlineCheckbox2">8월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month9">
   <label class="form-check-label" for="inlineCheckbox2">9월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month10">
   <label class="form-check-label" for="inlineCheckbox2">10월</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="month11">
   <label class="form-check-label" for="inlineCheckbox2">11월</label>
 </div>
 
 
 
 <br>
+<div class="d-flex">
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="all_subject">
+        <label class="form-check-label" for="inlineCheckbox2">전체</label>
+    </div>
 
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-  <label class="form-check-label" for="inlineCheckbox2">전체</label>
+    <div class="dropdown">
+        <button style="border-color: #bbbbc2;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            국어 
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">화법과 작문</a></li>
+            <li><a class="dropdown-item" href="#">언어와 매체</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <button style="border-color: #bbbbc2;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            수학 
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">확률과 통계</a></li>
+            <li><a class="dropdown-item" href="#">미적분</a></li>
+            <li><a class="dropdown-item" href="#">기하</a></li>
+        </ul>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="subject_eng">
+        <label class="form-check-label" for="inlineCheckbox2">영어</label>
+    </div>
+
 </div>
 
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+<!-- <div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="subject_kor">
   <label class="form-check-label" for="inlineCheckbox2">국어</label>
+
+  
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="subject_math">
   <label class="form-check-label" for="inlineCheckbox2">수학</label>
-</div>
+</div> -->
+
 
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-  <label class="form-check-label" for="inlineCheckbox2">영어</label>
-</div>
-
-<div class="form-check form-check-inline">
-  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="subject_history">
   <label class="form-check-label" for="inlineCheckbox2">한국사</label>
 </div>
+
+
+
+
+
+<div class="d-flex">
    <div class="dropdown">
-  <button class="btn btn-hover:hover dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <button style="border-color: #bbbbc2;" class="btn btn-hover:hover dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     사회탐구
   </button>
   <ul class="dropdown-menu">
@@ -89,8 +161,10 @@
 
 </ul>
 
+
+
 <div class="dropdown">
-  <button class="btn border-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <button style="border-color: #bbbbc2;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     과학탐구
   </button>
   <ul class="dropdown-menu">
@@ -105,7 +179,11 @@
  
   </ul>
 </div>
+</div>
+
+
 </template>
+
 
 <script setup>
 
@@ -115,6 +193,10 @@
   .form-check-input[type="checkbox"]:checked {
         background-color: #FF4D00;
         border-color: #FF4D00;
+    }
+    .form-check-input,
+    .form-check-label {
+        font-size: 20px; /* 원하는 크기로 조정하세요 */
     }
 
 </style>
