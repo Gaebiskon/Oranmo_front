682634f9045f26ad6913a2fc344c5a2a313df724
dksrud09 <166790071+dksrud09@users.noreply.github.com>
2024-05-08 15:40:42 +0900

    메뉴 상단바 생성

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ArticleView from '../views/ArticleView.vue'
 import ArticleCreateView from '../views/ArticleCreateView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,14 @@ const router = createRouter({
       name: 'articles-create',
       component: ArticleCreateView
     },
+    {
+      path: '/articles/login',
+      name: 'login',
+      component: LoginView
+    },
+
   ]
+
 })
 
 export default router
