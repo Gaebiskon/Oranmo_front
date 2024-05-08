30823588dd249a10862e3e8309561d50d508103c
woosoobin <166790070+woosoobin@users.noreply.github.com>
2024-05-08 15:50:43 +0900

    체크박스, 드롭다운 생성

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,121 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+    <div> </div>
+    <div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+  <label class="form-check-label" for="inlineCheckbox1">전체</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">3월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">4월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">5월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">6월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">7월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">8월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">9월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">10월</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">11월</label>
+</div>
+
+
+
+<br>
+
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">전체</label>
+</div>
+
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">국어</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">수학</label>
+</div>
+
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">영어</label>
+</div>
+
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">한국사</label>
+</div>
+   <div class="dropdown">
+  <button class="btn btn-hover:hover dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    사회탐구
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">생활과 윤리</a></li>
+    <li><a class="dropdown-item" href="#">윤리와 사상</a></li>
+    <li><a class="dropdown-item" href="#">한국지리</a></li>
+    <li><a class="dropdown-item" href="#">세계지리</a></li>
+    <li><a class="dropdown-item" href="#">동아시아사</a></li>
+    <li><a class="dropdown-item" href="#">세계사</a></li>
+    <li><a class="dropdown-item" href="#">경제</a></li>
+    <li><a class="dropdown-item" href="#">정치와 법</a></li>
+    <li><a class="dropdown-item" href="#">사회·문화</a></li>
+  </ul>
+</div>
+
+<ul>
+
+</ul>
+
+<div class="dropdown">
+  <button class="btn border-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    과학탐구
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">물리학Ⅰ</a></li>
+    <li><a class="dropdown-item" href="#">화학Ⅰ</a></li>
+    <li><a class="dropdown-item" href="#">생명과학Ⅰ</a></li>
+    <li><a class="dropdown-item" href="#">지구과학Ⅰ</a></li>
+    <li><a class="dropdown-item" href="#">물리학Ⅱ</a></li>
+    <li><a class="dropdown-item" href="#">화학Ⅱ</a></li>
+    <li><a class="dropdown-item" href="#">생명과학Ⅱ</a></li>
+    <li><a class="dropdown-item" href="#">지구과학Ⅱ</a></li>
+ 
+  </ul>
+</div>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
+<style >
+  .form-check-input[type="checkbox"]:checked {
+        background-color: #FF4D00;
+        border-color: #FF4D00;
+    }
 
 </style>
+
